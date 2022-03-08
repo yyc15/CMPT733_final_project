1c0be31bb1f5526619a5fedde3347567677a9404
joannzhang00 <wza105@sfu.ca>
2022-03-07 21:29:49 -0800

    Add Canada data and Time Zone

--- Preprocess_EDA.py
+++ Preprocess_EDA.py
@@ -3,13 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
-import datetime
+from datetime import datetime
+import pytz
+import os
 # %matplotlib inline # uncomment for Jupyter
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_colwidth', None)
-
 
 # extract category and corresponding id out of json file
 def extract_catid_js(jsfile):
@@ -28,26 +30,51 @@ def merge_cat(maindf, catdf):
     df = maindf.merge(catdf, how = 'left', on = 'categoryId')
     return df
 
+# read the data of a specific country, add country column, join the category and return a df
+def read_merge(filename, country):
+    df = pd.read_csv(os.path.join('data', filename))
+    # drop meaningless columns
+    df1 = df.drop(['channelId', 'thumbnail_link'], axis = 'columns')
+    # add country column
+    df1['country'] = country
+    # merge with cat id
+    cat = extract_catid_js('data/US_category_id.json')
+    merge_df = merge_cat(df1, cat)
+    return merge_df
 
-# combine the US & GB datasets
+# convert UTC (+00:00) Time zone to eastern time zone (considering Day Light Saving)
+def changeTimeZone(time):
+    utc = datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ')
+    timezone = pytz.timezone("UTC")
+    utc = timezone.localize(utc)
+    eastern_timezone = pytz.timezone("US/Eastern")
+    output_time = utc.astimezone(eastern_timezone)
+    return output_time
+
+# combine & clean the US & CA & GB datasets
 def integrate_clean_data():
-    us = pd.read_csv('US_youtube_trending_data.csv')
-    us['country'] = 'US'
-    uscat = extract_catid_js('US_category_id.json')
-    us_merge = merge_cat(us, uscat)
+    # read US data
+    us = read_merge('US_youtube_trending_data.csv', 'US')
+    # convert UTC (+00:00) Time zone to eastern time zone for US & CA
+    us['publishedAt'] = us['publishedAt'].apply(changeTimeZone)
 
-    gb = pd.read_csv('GB_youtube_trending_data.csv')
-    gb['country'] = 'GB'
-    #     gbcat = extract_catid_js('GB_category_id.json') # data deficiency
-    gb_merge = merge_cat(gb, uscat)
+    # read CA data
+    ca = read_merge('CA_youtube_trending_data.csv', 'CA')
+    # convert UTC (+00:00) Time zone to eastern time zone for US & CA
+    ca['publishedAt'] = ca['publishedAt'].apply(changeTimeZone)
+
+    # read GB data
+    gb = read_merge('GB_youtube_trending_data.csv', 'GB')
+    # no time zone conversion for Great Britain
+    gb['publishedAt'] = gb['publishedAt'].apply(lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ'))
 
     # merge two countries
-    df_list = [us_merge, gb_merge]
+    df_list = [us, ca, gb]
     df = pd.concat(df_list).reset_index(drop=True)
 
     # create date and time features
-    df['publishDate'] = pd.to_datetime(df['publishedAt']).dt.date
-    df['publishTime'] = pd.to_datetime(df['publishedAt']).dt.time
+    df['publishDate'] = df['publishedAt'].apply(lambda x: x.date())
+    df['publishTime'] = df['publishedAt'].apply(lambda x: x.time())
     df['trendingDate'] = pd.to_datetime(df['trending_date']).dt.date  # there was no trending time
 
     # subset data from 2021-01-01
@@ -58,11 +85,7 @@ def integrate_clean_data():
     df['title_lst'] = df['title'].str.strip(' *#\'\",$@!\n\t').str.split(pat="|")
     df['tags_lst'] = df['tags'].str.strip(' *#\'\",$@!\n\t').str.split(pat="|")
 
-    # drop meaningless columns
-    df1 = df.drop(['channelId', 'thumbnail_link'], axis='columns')
+    return df
 
-    return df1
-
-# process with he youtube trending data (since the saved csv file distorts result)
 df = integrate_clean_data()
 print(df.head(3))
